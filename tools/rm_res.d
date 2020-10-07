71540329e72c665c4540a118995cc6eb2104c282
Andreas <andreas.fueglistaler@gmail.com>
2020-10-07 15:01:06 +0200

    corrected module name

--- tools/rm_res.d
+++ tools/rm_res.d
@@ -13,7 +13,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-module tools.extract;
+module tools.rm_res;
 
 auto str2index(string s) {
 	import std.array;
